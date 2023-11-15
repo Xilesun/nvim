d957fbe60f6a6c0b5efeab7c545cd55dc3db8f94
xilesun <2013xile@gmail.com>
2023-11-15 10:33:34 +0800

    use copilot as cmp

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,40 +11,75 @@ return {
   -- },
   {"olimorris/onedarkpro.nvim", priority = 1000}, {
     "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+    -- cmd = "Copilot",
+    -- event = "InsertEnter",
     config = function()
       require("copilot").setup({
-        suggestion = {auto_trigger = true, accept = false}
+        suggestion = {suggestion = {enabled = false}, panel = {enabled = false}}
       })
     end
   }, {
+    "zbirenbaum/copilot-cmp",
+    event = {"InsertEnter", "LspAttach"},
+    dependencies = {"zbirenbaum/copilot.lua"},
+    config = function() require("copilot_cmp").setup() end
+  }, {
     "hrsh7th/nvim-cmp",
+    dependencies = {"zbirenbaum/copilot-cmp"},
     opts = function(_, opts)
       -- opts parameter is the default options table
       -- the function is lazy loaded so cmp is able to be required
       local cmp = require "cmp"
       local luasnip = require "luasnip"
+      local lspkind = require "lspkind"
+      -- local has_words_before = function()
+      --   local cursor = vim.api.nvim_win_get_cursor(0)
+      --   return
+      --       (vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or
+      --           ''):sub(cursor[2], cursor[2]):match('%s')
+      -- end
       local has_words_before = function()
-        local cursor = vim.api.nvim_win_get_cursor(0)
-        return
-            (vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or
-                ''):sub(cursor[2], cursor[2]):match('%s')
+        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+          return false
+        end
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and
+                   vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match(
+                       "^%s*$") == nil
       end
+      opts.sources = cmp.config.sources {
+        {name = "nvim_lsp", priority = 1000},
+        {name = "luasnip", priority = 750}, {name = "buffer", priority = 500},
+        {name = "path", priority = 250}, {name = "copilot", priority = 1250}
+      }
+      opts.formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol",
+          max_width = 50,
+          symbol_map = {Copilot = ""}
+        })
+      }
       -- modify the mapping part of the table
-      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
-        if require("copilot.suggestion").is_visible() then
-          require("copilot.suggestion").accept()
-        elseif cmp.visible() then
-          cmp.select_next_item({behavior = cmp.SelectBehavior.Insert})
-        elseif luasnip.expandable() then
-          luasnip.expand()
-        elseif has_words_before() then
-          cmp.complete()
+      -- opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+      --   if require("copilot.suggestion").is_visible() then
+      --     require("copilot.suggestion").accept()
+      --   elseif cmp.visible() then
+      --     cmp.select_next_item({behavior = cmp.SelectBehavior.Insert})
+      --   elseif luasnip.expandable() then
+      --     luasnip.expand()
+      --   elseif has_words_before() then
+      --     cmp.complete()
+      --   else
+      --     fallback()
+      --   end
+      -- end, {"i", "s"})
+      opts.mapping["<Tab>"] = vim.schedule_wrap(function(fallback)
+        if cmp.visible() and has_words_before() then
+          cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
         else
           fallback()
         end
-      end, {"i", "s"})
+      end)
       return opts
     end
   }, {"David-Kunz/jester"}, {
